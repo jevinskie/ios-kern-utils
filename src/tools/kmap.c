d0f9cd6859dd2c0f27afa5d4ff56e6681906cc3f
Siguza <siguza@siguza.net>
2017-02-18 18:52:31 +0100

    Add extended options to kmap

--- src/tools/kmap.c
+++ src/tools/kmap.c
@@ -5,17 +5,20 @@
  * Copyright (c) 2016-2017 Siguza
  */
 
+#include <stdbool.h>            // bool, true, false
 #include <stdio.h>              // printf, fprintf, stderr
 
 #include <mach/kern_return.h>   // KERN_SUCCESS, kern_return_t
 #include <mach/mach_types.h>    // task_t
 #include <mach/message.h>       // mach_msg_type_number_t
+#include <mach/vm_inherit.h>    // VM_INHERIT_*
 #include <mach/vm_map.h>        // vm_region_recurse_64
 #include <mach/vm_prot.h>       // VM_PROT_READ, VM_PROT_WRITE, VM_PROT_EXECUTE
 #include <mach/vm_region.h>     // VM_REGION_SUBMAP_INFO_COUNT_64, vm_region_info_t, vm_region_submap_info_data_64_t
 #include <mach/vm_types.h>      // vm_address_t, vm_size_t
 
 #include "arch.h"               // ADDR
+#include "debug.h"              // slow, verbose
 #include "libkern.h"            // get_kernel_task
 
 static const char* share_mode(char mode)
@@ -34,8 +37,60 @@ static const char* share_mode(char mode)
     return "???";
 }
 
-int main(void)
+static const char* inheritance(vm_inherit_t inh)
 {
+    switch(inh)
+    {
+        case VM_INHERIT_SHARE:          return "sh";
+        case VM_INHERIT_COPY:           return "cp";
+        case VM_INHERIT_NONE:           return "--";
+        case VM_INHERIT_DONATE_COPY:    return "dn";
+    }
+    return "??";
+}
+
+static void print_usage(const char *self)
+{
+    fprintf(stderr, "Usage: %s [-h] [-v [-d]] [-e]\n"
+                    "    -d  Debug mode (sleep between function calls, gives\n"
+                    "        sshd time to deliver output before kernel panic)\n"
+                    "    -e  Extended output (print all information available)\n"
+                    "    -h  Print this help\n"
+                    "    -v  Verbose (debug output)\n"
+                    , self);
+}
+
+int main(int argc, const char **argv)
+{
+    bool extended = false;
+
+    for(int i = 1; i < argc; ++i)
+    {
+        if(strcmp(argv[i], "-h") == 0)
+        {
+            print_usage(argv[0]);
+            return 0;
+        }
+        if(strcmp(argv[i], "-d") == 0)
+        {
+            slow = true;
+        }
+        else if(strcmp(argv[i], "-v") == 0)
+        {
+            verbose = true;
+        }
+        else if(strcmp(argv[i], "-e") == 0)
+        {
+            extended = true;
+        }
+        else
+        {
+            fprintf(stderr, "[!] Unrecognized option: %s\n\n", argv[i]);
+            print_usage(argv[0]);
+            return -1;
+        }
+    }
+
     task_t kernel_task;
 
     KERNEL_TASK_OR_GTFO(kernel_task);
@@ -79,10 +134,24 @@ int main(void)
         maxW = (info.max_protection) & VM_PROT_WRITE ? 'w' : '-';
         maxX = (info.max_protection) & VM_PROT_EXECUTE ? 'x' : '-';
 
-        printf(ADDR "-" ADDR " [%5zu%c] %c%c%c/%c%c%c [%s %s]\n"
-               , addr, addr+size, displaysize, scale
-               , curR, curW, curX, maxR, maxW, maxX
-               , info.user_tag > 0 ? "usr" : "krn", share_mode(info.share_mode));
+        if(extended)
+        {
+            printf(ADDR "-" ADDR " [%5zu%c] %c%c%c%c/%c%c%c%c [%s %s %s] %016llx [%u %u %hu %hhu %hu] %08x/%08x:<%10u> %u,%u {%u,%u}\n"
+                   , addr, addr+size, displaysize, scale
+                   , (info.protection     & ~(VM_PROT_ALL)) ? '+' : '-', curR, curW, curX
+                   , (info.max_protection & ~(VM_PROT_ALL)) ? '+' : '-', maxR, maxW, maxX
+                   , info.is_submap ? "map" : "mem", share_mode(info.share_mode), inheritance(info.inheritance), info.offset
+                   , info.behavior, info.pages_reusable, info.user_wired_count, info.external_pager, info.shadow_depth // these should all be 0
+                   , info.user_tag, info.object_id, info.ref_count
+                   , info.pages_swapped_out, info.pages_shared_now_private, info.pages_resident, info.pages_dirtied
+            );
+        }
+        else
+        {
+            printf(ADDR "-" ADDR " [%5zu%c] %c%c%c/%c%c%c\n"
+                   , addr, addr+size, displaysize, scale
+                   , curR, curW, curX, maxR, maxW, maxX);
+        }
     }
 
     return 0;
